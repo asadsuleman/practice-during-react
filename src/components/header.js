42478e8f8c66d553fa4f5df4480a7fb7da790ab7
asadsuleman <65735006+asadsuleman@users.noreply.github.com>
2020-10-18 02:14:23 +0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react';
 const header = () =>{
   
     return <div className='div1'>
-        <h1>header of the page</h1>
+        <h1>header of the main page</h1>
     </div>
 }
 
