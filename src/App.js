3cce1ff8e5ee8f6cf97b6c32786badd0e75476b8
asadsuleman <65735006+asadsuleman@users.noreply.github.com>
2020-10-22 02:03:03 +0500

    useState learning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Mycomponent1 from './MyComponent'
@@ -7,12 +7,22 @@ import Header from './components/header';
 import Footer from './components/footer';
 
 function App() {
+  
+  const [count , setCount]= useState(0);
   return (
     <div className="App">
     <Mycomponent1 text="its a text parent to child"></Mycomponent1>
 
     <Header/>
     <Footer/>
+    
+        {/* state and hooks */}
+        <h1>Count : {count}</h1>
+
+       <button onClick={() => setCount(count +1)}>Increase Count</button>
+        <button onClick={() => setCount(count - 1)}>decrease Count</button>
+
+
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import  Css from './style.css'  
+
 
 
 
@@ -6,6 +8,7 @@ const header = () =>{
   
     return <div className='div1'>
         <h1>header of the page</h1>
+        
     </div>
 }
 
